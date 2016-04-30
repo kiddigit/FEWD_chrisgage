9f159dda7b894e2cee42ecb4585ba05601050749
kiddigit <chrisgage@Chriss-MacBook-Pro.local>
2016-04-30 11:26:52 -0400

    a

--- week3_CG/Class_lab/css/form.js
+++ week3_CG/Class_lab/css/form.js
@@ -1,18 +1,22 @@
- function validate()
-      {
-      
-         if( document.myForm.firstname.value == "" )
-         {
-            alert( "Please provide your name!" );
-            document.myForm.firstname.focus() ;
-            return false;
-         }
-         
-         if( document.myForm.lastname.value == "" )
-         {
-            alert( "Please provide your Email!" );
-            document.myForm.lastname.focus() ;
-            return false;
-         }
-      }
-   //-->
+  function checkForm(form)
+  {
+    // validation fails if the input is blank
+    if(form.inputfield.value == "") {
+      alert("Error: Input is empty!");
+      form.inputfield.focus();
+      return false;
+    }
+
+    // regular expression to match only alphanumeric characters and spaces
+    var re = /^[\w ]+$/;
+
+    // validation fails if the input doesn't match our regular expression
+    if(!re.test(form.inputfield.value)) {
+      alert("Error: Input contains invalid characters!");
+      form.inputfield.focus();
+      return false;
+    }
+
+    // validation was successful
+    return true;
+  }
